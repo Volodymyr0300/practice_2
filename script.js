8e4168b48a41b22894e2971676bedbdce4b751c5
Volodymyr Poliakov <vladimir03002@gmail.com>
2023-05-31 14:23:41 -0500

    some practice

---
+++ script.js
@@ -0,0 +1,48 @@
+// function friend(friends) {
+//   //your code here
+//   return friends.filter((friend) => friend.length === 4);
+// }
+// !
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+// The output should be two capital letters with a dot separating them.
+// It should look like this:
+// Sam Harris => S.H
+// patrick feeney => P.F
+
+// function abbrevName(name) {
+//   // code away
+//   return `${name[0].toUpperCase()}.${name[
+//     name.indexOf(" ") + 1
+//   ].toUpperCase()}`;
+// }
+// console.log(abbrevName("patrick feeney"));
+// //   var nameArray = name.split(" ");
+// //   return (nameArray[0][0] + "." + nameArray[1][0]).toUpperCase();
+// !
+
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
+// !
